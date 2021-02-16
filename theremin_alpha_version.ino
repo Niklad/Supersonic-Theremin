2b12af6a2f5831c1b064baa714e43b2d32e3b138
Niklad <60095505+Niklad@users.noreply.github.com>
2021-02-16 20:03:28 +0100

    Minor changes

--- theremin_alpha_version.ino
+++ theremin_alpha_version.ino
@@ -1,19 +1,19 @@
 #include <RunningMedian.h>
 
 byte playPinLow = 2;     // button pin
-byte playPinMid = 3;    // button pin
+byte playPinMid = 3;     // button pin
 byte playPinHigh = 4;    // button pin
 byte trigPin = 11;       // Trigger
 byte echoPin = 12;       // Echo
 
 // change minRange and maxRange depending on the usable range of your sensor:
-unsigned int minRange = 100;
-unsigned int maxRange = 2000;       
-float lowMinFreq = 41.20;    // E1
+unsigned int minRange = 300;          // set a few cm away from the sensor, get too close and it will be bad
+unsigned int maxRange = 2000;         // set to a distance that won't cause too many false negatives
+float lowMinFreq = 41.20;             // E1
 float lowMaxFreq = 2 * lowMinFreq;    // E2
-float midMinFreq = lowMaxFreq;   // E2
-float midMaxFreq = 2 * midMinFreq;   // E3
-float highMinFreq = midMaxFreq;   // E3
+float midMinFreq = lowMaxFreq;        // E2
+float midMaxFreq = 2 * midMinFreq;    // E3
+float highMinFreq = midMaxFreq;       // E3
 float highMaxFreq = 2 * midMaxFreq;   // E4
 
 byte bufferSize = 19; // size of buffer window, higher bufferSize should give more stable tone but more latency
@@ -40,24 +40,25 @@ void loop() {
     digitalWrite(trigPin, LOW);  
     
     samples.add(pulseIn(echoPin, HIGH)); // add pulse length to samples
-    long median = samples.getMedian(); // calculate the median of buffered samples for stability
+    long median = samples.getMedian();   // calculate the median of buffered samples for stability
     int frequency{};
     // Use different tone range depending on button input
-    if (digitalRead(playPinLow) && median <= maxRange && median >= minRange){
-        frequency = map(median, minRange, maxRange, lowMinFreq, lowMaxFreq);
+    if (digitalRead(playPinLow)){
+        frequency = constrain(map(median, minRange, maxRange, lowMinFreq, lowMaxFreq), lowMinFreq, lowMaxFreq);
     }
-    if (digitalRead(playPinMid) && median <= maxRange && median >= minRange){
-        frequency = map(median, minRange, maxRange, midMinFreq, midMaxFreq);
+    if (digitalRead(playPinMid)){
+        frequency = constrain(map(median, minRange, maxRange, midMinFreq, midMaxFreq), midMinFreq, midMaxFreq);
     }
-    if (digitalRead(playPinHigh) && median <= maxRange && median >= minRange){
-        frequency = map(median, minRange, maxRange, highMinFreq, highMaxFreq);
+    if (digitalRead(playPinHigh)){
+        frequency = constrain(map(median, minRange, maxRange, highMinFreq, highMaxFreq), highMinFreq, highMaxFreq);
     }
 
     tone(9, frequency, 16); // play the pitch:
-    delay(1); // delay in between reads for stability, might not be needed
+    delay(1);               // delay in between reads for stability, might not be needed
   }
   else {
-    noTone(9); // don't play a tone
-    delay(1); // might not be needed, will test later
+    noTone(9);        // don't play a tone
+    samples.clear();  // clear the buffer between plays 
+    delay(1);         // might not be needed, will test later
   } 
 }
