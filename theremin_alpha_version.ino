cacb4682e70831cc121c188f48b17f90b92bf3b7
Niklad <60095505+Niklad@users.noreply.github.com>
2021-02-16 13:52:20 +0100

    Minor changes

--- theremin_alpha_version.ino
+++ theremin_alpha_version.ino
@@ -1,9 +1,9 @@
 #include <RunningMedian.h>
 
-int playPinLow = 2;     // button pin
-int playPinHigh = 3;    // button pin
-int trigPin = 11;       // Trigger
-int echoPin = 12;       // Echo
+byte playPinLow = 2;     // button pin
+byte playPinHigh = 3;    // button pin
+byte trigPin = 11;       // Trigger
+byte echoPin = 12;       // Echo
 
 unsigned int minRange = 200;
 unsigned int maxRange = 4000;       
@@ -12,16 +12,17 @@ float lowMaxFreq = 82.41;    // E2
 float highMinFreq = 82.41;   // E2
 float highMaxFreq = 164.8;   // E3
 
-// make a buffer with recent samples
-unsigned int windowSize = 19;
-RunningMedian samples = RunningMedian(windowSize);
+byte windowSize = 19; // size of buffer window
+RunningMedian samples = RunningMedian(windowSize); // buffer of size windowSize
 
 void setup() {
-    // initialize serial communications (for debugging only):
-    Serial.begin(9600);
-    // define inputs and outputs
+    Serial.begin(9600); // initialize serial communications (for debugging only)
+    
+    // defining inputs and outputs:
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
+    pinMode(playPinLow, INPUT);
+    pinMode(playPinHigh, INPUT);
 }
 
 void loop() {
@@ -33,29 +34,23 @@ void loop() {
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);  
     
-    // add pulse length to samples
-    samples.add(pulseIn(echoPin, HIGH));
-    // calculate the median of buffered samples for stability
-    long median = samples.getMedian();
+    samples.add(pulseIn(echoPin, HIGH)); // add pulse length to samples
+    long median = samples.getMedian(); // calculate the median of buffered samples for stability
     // change the minimum and maximum input numbers below depending on the range
     // your sensor's giving:
     int frequency{};
     if (digitalRead(playPinLow) && median <= maxRange){
         frequency = map(median, minRange, maxRange, lowMinFreq, lowMaxFreq);
     }
-    else if (digitalRead(playPinHigh) && median <= maxRange){
+    if (digitalRead(playPinHigh) && median <= maxRange){
         frequency = map(median, minRange, maxRange, highMinFreq, highMaxFreq);
     }
-        
-    // play the pitch:
-    tone(9, frequency, 16);
-    // delay in between reads for stability
-    delay(1);        
+
+    tone(9, frequency, 16); // play the pitch:
+    delay(1); // delay in between reads for stability, might not be needed
   }
   else {
-    noTone(9);
-    // start with a clear buffer after stopping
-    samples.clear();
-    delay(1);
+    noTone(9); // don't play a tone
+    delay(1); // might not be needed, will test later
   } 
 }
